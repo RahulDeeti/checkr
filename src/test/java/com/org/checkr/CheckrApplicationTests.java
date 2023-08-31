765b09245c27bdaa78466a2c2e126ef9bfe54fa8
RahulDeeti <88383183+RahulDeeti@users.noreply.github.com>
2023-08-31 11:45:41 +0530

    Revert "test: unit testing the application"

--- src/test/java/com/org/checkr/CheckrApplicationTests.java
+++ src/test/java/com/org/checkr/CheckrApplicationTests.java
@@ -1,7 +1,10 @@
 package com.org.checkr;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+//@SpringBootTest
 class CheckrApplicationTests {
 
 	@Test
